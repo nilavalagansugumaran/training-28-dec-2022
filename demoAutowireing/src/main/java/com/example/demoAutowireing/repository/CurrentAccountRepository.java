8ab27c5e9fc29eb2bb8ecc9f4bac7689706b6bab
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-11-28 15:33:18 +0000

    application arguments

--- demoAutowireing/src/main/java/com/example/demoAutowireing/repository/CurrentAccountRepository.java
+++ demoAutowireing/src/main/java/com/example/demoAutowireing/repository/CurrentAccountRepository.java
@@ -1,12 +1,23 @@
 package com.example.demoAutowireing.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CurrentAccountRepository implements BankRepository{
 
+    private ApplicationArguments applicationArguments;
+
+    @Autowired
+    public CurrentAccountRepository(ApplicationArguments applicationArguments) {
+        this.applicationArguments = applicationArguments;
+    }
+
     @Override
     public void doWithdraw() {
         System.out.println("Current account - withdraw");
+        System.out.println(applicationArguments.getNonOptionArgs());
+        System.out.println(applicationArguments.getOptionNames());
     }
 }
