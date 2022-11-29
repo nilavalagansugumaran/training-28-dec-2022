40d5bb89712791256f0cb43ddefed40f1be6b4db
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-11-29 16:25:06 +0000

    update and delete employee

--- demoRestService/src/main/java/com/example/demoRestService/controllers/EmployeeController.java
+++ demoRestService/src/main/java/com/example/demoRestService/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.demoRestService.controllers;
 import com.example.demoRestService.models.Employee;
 import com.example.demoRestService.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,10 +17,11 @@ public class EmployeeController {
 
     //create
     @PostMapping(path = "/employee", produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml"})
+    @ResponseStatus(HttpStatus.CREATED)
     public void createEmployee(@RequestBody Employee employee){
         service.createEmployee(employee);
     }
-    
+
     //read
     @GetMapping(path = "/employee/{id}", produces = {"application/json", "application/xml"})
     public Employee getEmployee(@RequestParam( name = "name", required = false) String name,@PathVariable("id") int id){
@@ -29,8 +31,17 @@ public class EmployeeController {
         return service.getEmployeeById(id);
     }
 
-
     //update
-
+    @PutMapping(path = "/employee/{id}", produces = {"application/json", "application/xml"}, consumes = {"application/json", "application/xml"})
+    @ResponseStatus(HttpStatus.OK)
+    public void updateEmployee(@RequestBody Employee employee, @PathVariable("id") int id){
+        service.updateEmployee(id, employee);
+    }
+    
     //delete
+    @DeleteMapping(path = "/employee/{id}", produces = {"application/json", "application/xml"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable("id") int id){
+        service.deleteEmployee(id);
+    }
 }
