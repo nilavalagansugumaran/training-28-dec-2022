d578c7040dd09ea81e47c3c75d2c69bb455a123c
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-11-29 15:08:04 +0000

    Path variable

--- demoRestService/src/main/java/com/example/demoRestService/controllers/EmployeeController.java
+++ demoRestService/src/main/java/com/example/demoRestService/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.demoRestService.controllers;
 
 import com.example.demoRestService.models.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,10 @@ public class EmployeeController {
     //create
 
     //read
-    @GetMapping(path = "/employee", produces = {"application/json", "application/xml"})
-    public Employee getEmployee(@RequestParam( name = "name", required = false) String name){
+    @GetMapping(path = "/employee/{id}", produces = {"application/json", "application/xml"})
+    public Employee getEmployee(@RequestParam( name = "name", required = false) String name,@PathVariable("id") int id){
 
-        return Employee.builder().id(1).name(name).salary(2000.00).email("nila@nila.com").build();
+        return Employee.builder().id(id).name(name).salary(2000.00).email("nila@nila.com").build();
     }
 
 
