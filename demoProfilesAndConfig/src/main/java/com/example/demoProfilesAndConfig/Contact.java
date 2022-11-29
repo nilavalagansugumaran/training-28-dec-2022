34bdec0569852818b27da92dd1fe63c70fbb224d
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-11-29 09:52:01 +0000

    with lombok

--- demoProfilesAndConfig/src/main/java/com/example/demoProfilesAndConfig/Contact.java
+++ demoProfilesAndConfig/src/main/java/com/example/demoProfilesAndConfig/Contact.java
@@ -1,20 +1,18 @@
 package com.example.demoProfilesAndConfig;
 
+import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Contact {
     @Value("${name}")
     private String name;
     @Value("${email}")
     private String email;
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
--- demoProfilesAndConfig/src/main/java/com/example/demoProfilesAndConfig/DemoProfilesAndConfigApplication.java
+++ demoProfilesAndConfig/src/main/java/com/example/demoProfilesAndConfig/DemoProfilesAndConfigApplication.java
@@ -1,16 +1,19 @@
 package com.example.demoProfilesAndConfig;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class DemoProfilesAndConfigApplication {
 
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(DemoProfilesAndConfigApplication.class, args);
-		System.out.println(ctx.getBean(Contact.class).toString());
+		//System.out.println(ctx.getBean(Contact.class).toString());
+		log.info("this contact bean {}",ctx.getBean(Contact.class).toString() );
 	}
 
 }
