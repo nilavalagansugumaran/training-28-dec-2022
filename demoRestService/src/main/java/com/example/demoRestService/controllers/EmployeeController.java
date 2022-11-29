4d453a61a3da9f951dd21cb58ddd419e7e71e11e
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-11-29 15:03:49 +0000

    with request param

--- demoRestService/src/main/java/com/example/demoRestService/controllers/EmployeeController.java
+++ demoRestService/src/main/java/com/example/demoRestService/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.demoRestService.controllers;
 
 import com.example.demoRestService.models.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,9 @@ public class EmployeeController {
 
     //read
     @GetMapping(path = "/employee", produces = {"application/json", "application/xml"})
-    public Employee getEmployee(){
+    public Employee getEmployee(@RequestParam( name = "name", required = false) String name){
 
-        return Employee.builder().id(1).name("nila").salary(2000.00).email("nila@nila.com").build();
+        return Employee.builder().id(1).name(name).salary(2000.00).email("nila@nila.com").build();
     }
 
 
