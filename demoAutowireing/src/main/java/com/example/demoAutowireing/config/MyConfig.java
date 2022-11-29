6abbe36cc204a036e82c164861522c026b8aefb6
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-11-29 10:59:23 +0000

    Actuator example

--- demoAutowireing/src/main/java/com/example/demoAutowireing/config/MyConfig.java
+++ demoAutowireing/src/main/java/com/example/demoAutowireing/config/MyConfig.java
@@ -1,13 +1,16 @@
 package com.example.demoAutowireing.config;
 
 import com.example.demoAutowireing.MyBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfig {
 
-    @Bean("new-name")
+    @Bean
+    @ConditionalOnWebApplication
     public MyBean myBean(){
 
         return new MyBean();
