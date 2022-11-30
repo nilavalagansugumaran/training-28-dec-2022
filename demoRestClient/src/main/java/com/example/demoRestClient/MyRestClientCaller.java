ee8e0e317ac4bbde71c4e32da263684ca53b73d6
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-11-30 10:13:45 +0000

    Add parametrised type for client

--- demoRestClient/src/main/java/com/example/demoRestClient/MyRestClientCaller.java
+++ demoRestClient/src/main/java/com/example/demoRestClient/MyRestClientCaller.java
@@ -1,8 +1,14 @@
 package com.example.demoRestClient;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -18,6 +24,11 @@ public class MyRestClientCaller implements MyRestClient {
         Employee employee = restTemplate.getForObject("http://localhost:8080/employee/102", Employee.class);
         log.info("Employee from service {}", employee);
 
+        ParameterizedTypeReference<List<Employee>> employeeListType = new ParameterizedTypeReference<List<Employee>>() {};
+        ResponseEntity<List<Employee>> response = restTemplate.exchange("http://localhost:8080/employee", HttpMethod.GET, null, employeeListType );
+        log.info("Employee list from service status code = {}", response.getStatusCode());
+        log.info("Employee list from service  body = {}", response.getBody());
+        log.info("Employee list from service headers = {}", response.getHeaders().values().stream().collect(Collectors.toList()));
 
     }
 }
