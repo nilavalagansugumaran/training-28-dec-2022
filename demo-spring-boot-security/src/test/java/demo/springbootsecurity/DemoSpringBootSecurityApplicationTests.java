573f22bbf68019b6be1bde5a3690476605fe5ef0
Nilavalagan Sugumaran <nilavalagan.sugumaran@pay4later.com>
2022-12-01 14:29:47 +0000

    Demo spring security

---
+++ demo-spring-boot-security/src/test/java/demo/springbootsecurity/DemoSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
